30b20e183b537582e32cd3c3d083f70d7eed7b80
jerpelhan <36811160+jerpelhan@users.noreply.github.com>
2024-06-12 08:13:46 +0200

    Update demo.py
    Updated demo.

--- demo.py
+++ demo.py
@@ -1,20 +1,17 @@
 from torch.nn import DataParallel
 from models.dave import build_model
 from utils.arg_parser import get_argparser
-import os
 import argparse
 import torch
-from torchvision import transforms as T
 
 import matplotlib.patches as patches
 from PIL import Image
 
 from utils.data import resize
 import matplotlib.pyplot as plt
-import matplotlib
-print(matplotlib.rcParams['backend'])
 
 bounding_boxes = []
+
 
 def on_click(event):
     # Record the starting point of the bounding box
@@ -22,6 +19,7 @@ def on_click(event):
     ix, iy = event.xdata, event.ydata
     # Connect the release event
     fig.canvas.mpl_connect('button_release_event', on_release)
+
 
 def on_release(event):
     # Record the ending point of the bounding box
@@ -34,12 +32,13 @@ def on_release(event):
     rect = patches.Rectangle((ix, iy), width, height, edgecolor='r', facecolor='none')
     ax.add_patch(rect)
     # Store the bounding box coordinates
-    bounding_boxes.append((ix, iy, width, height))
+    bounding_boxes.append((ix, iy, ix + width, iy + height))
     plt.draw()
+
 
 @torch.no_grad()
 def demo(args):
-    img_path = "material//7.jpg"
+    img_path = "results//7.jpg"
     global fig, ax
 
     gpu = 0
@@ -51,38 +50,39 @@ def demo(args):
         device_ids=[gpu],
         output_device=gpu
     )
-    state_dict = torch.load(os.path.join(args.model_path, f'{args.model_name}.pth'))['model']
+    state_dict = torch.load('results/DAVE_3_shot.pth')['model']
     state_dict = {k if 'module.' in k else 'module.' + k: v for k, v in state_dict.items()}
     model.load_state_dict(state_dict, strict=False)
     model.eval()
 
-
-
-    image = image = Image.open(img_path).convert("RGB")
+    image = Image.open(img_path).convert("RGB")
     # Create a figure and axis
     fig, ax = plt.subplots(1)
     ax.imshow(image)
     # Connect the click event
     cid = fig.canvas.mpl_connect('button_press_event', on_click)
+    plt.title("Click and drag to draw bboxes, then close window")
     # Show the image
     plt.show()
 
     bboxes = torch.tensor(bounding_boxes)
 
-    # resize and pad
-    img = T.ToTensor()(image)
-    bboxes = bboxes.to(device).unsqueeze(0)
-    r_image, r_bboxes, _,padwh = resize(img, bboxes)
+    img, bboxes, scale = resize(image, bboxes)
+    img = img.unsqueeze(0).to(device)
+    bboxes = bboxes.unsqueeze(0).to(device)
 
-    image = r_image.to(device).unsqueeze(0)
+    denisty_map, _, tblr, predicted_bboxes = model(img, bboxes=bboxes)
 
-    image = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(image)
+    plt.clf()
+    plt.imshow(image)
+    pred_boxes = predicted_bboxes.box.cpu() / torch.tensor([scale[0], scale[1], scale[0], scale[1]])
+    for i in range(len(pred_boxes)):
+        box = pred_boxes[i]
 
-    denisty_map, _, tblr, predicted_bboxes = model(image, bboxes=bboxes)
-
-
-
-
+        plt.plot([box[0], box[0], box[2], box[2], box[0]], [box[1], box[3], box[3], box[1], box[1]], linewidth=2,
+                 color='red')
+    plt.title("Dmap count:" + str(round(denisty_map.sum().item(), 1)) + " Box count:" + str(len(pred_boxes)))
+    plt.show()
 
 
 if __name__ == '__main__':
